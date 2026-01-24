9cf798829142f6f30a3fb272c0e87bf0e8f38501
hwoodall30 <hwoodall30@gmail.com>
2026-01-24 00:43:02 -0500

    Update footer icons

--- src/lib/components/app/footer.svelte
+++ src/lib/components/app/footer.svelte
@@ -15,15 +15,15 @@
 				href="mailto:contact@example.com"
 				class="rounded-md border border-orange-500/50 bg-linear-to-br from-orange-600/20 to-yellow-600/20 p-2"
 			>
-				<Icon icon="lucide:mail" class="h-3 w-3 sm:h-5 sm:w-5" />
+				<Icon icon="lucide:mail" class="h-4 w-4 text-orange-400 sm:h-5 sm:w-5" />
 			</a>
 
 			<a
 				target="_blank"
 				href="https://www.facebook.com/people/The-Village-Collaborative/61585969575739/"
-				class="rounded-md border border-blue-500/50 bg-linear-to-br from-cyan-600/20 to-blue-600/20 p-2"
+				class="rounded-md border border-blue-400/50 bg-linear-to-br from-cyan-700/20 to-blue-700/20 p-2 dark:border-blue-500/50 dark:from-cyan-600/20 dark:to-blue-600/20"
 			>
-				<Icon icon="lucide:facebook" class="h-3 w-3 sm:h-5 sm:w-5" />
+				<Icon icon="lucide:facebook" class="h-4 w-4 text-blue-400 sm:h-5 sm:w-5 " />
 			</a>
 
 			<a
@@ -31,7 +31,7 @@
 				href="https://maps.app.goo.gl/waCCPocKkGA2ckMp8"
 				class="rounded-md border border-green-500/50 bg-linear-to-br from-green-600/20 to-emerald-600/20 p-2"
 			>
-				<Icon icon="lucide:map-pin" class="h-3 w-3 sm:h-5 sm:w-5" />
+				<Icon icon="lucide:map-pin" class="h-4 w-4 text-emerald-500 sm:h-5 sm:w-5" />
 			</a>
 		</div>
 
